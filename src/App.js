ec9c5fd1ad031ca9d42578e12593dfa5b063dfa5
Manish Talmale <manishtalmale@gmail.com>
2023-06-19 15:10:00 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Customer from './component/Customer/Customer';
+import Pricing from './component/pricing/Pricing';
+import Signin from './component/signin/Signin';
+import Signup from './component/signup/Signup';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import Footer from './component/Footer/Footer';
 
 function App() {
+  const [Show, setShow] = useState(false)
+  const hideCompoenet = () => {
+    if (Show === true) {
+      setShow(true)
+    }
+    else {
+      setShow(false)
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Show && (<Header />)}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/customers' element={<Customer />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup hideCompoenet={hideCompoenet} />} />
+      </Routes>
+      <Footer />
+
     </div>
   );
 }
